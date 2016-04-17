4f5268a7b83eaa19beb7edeec3f0238181df231c
cutedili <dweerasinghe91@gmail.com>
2016-04-17 19:00:47 +0530

    Android Project

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?PHP
+echo "Hiii LeafOfLife";
+if(isset($_POST['image'])){
+    $now = DateTime::createFromFormat('U.u', microtime(true));
+    $id = $now->format('YmdHisu');
+    
+    $upload_folder = "upload";
+    $path = "$upload_folder/$id.jpg";
+    $image = $_POST['image'];
+    if(file_put_contents($path, base64_decode($image)) != false){
+        echo "uploaded_success";
+        exit;
+    }
+    else{
+        echo "uploaded_failed";
+        exit;
+    }
+}
+else{
+    echo "image_not_in";
+    exit;
+}
+
+?>
